39d20577ad032f800051f766f69e333fc4b4cb60
Vinicius Teixeira <teixeiravinicius32@gmail.com>
2024-10-24 17:32:07 -0300

    Inclusão de tokens em todas as requisições

--- src/app/services/agendamentos.service.ts
+++ src/app/services/agendamentos.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Agendamentos } from '../componentes/home/home.component';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,21 +11,28 @@ import { Agendamentos } from '../componentes/home/home.component';
 export class AgendamentosService {
   private apiUrl = 'https://sistema-agendamento-back.onrender.com/agendamentos'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
+
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('authToken')
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`)
+  }
 
   getAgendamentos(): Observable<Agendamentos[]> {
-    return this.http.get<Agendamentos[]>(`${this.apiUrl}`)
+    return this.http.get<Agendamentos[]>(`${this.apiUrl}`, { headers: this.getAuthHeaders() })
   }
 
   saveAgendamento(agendamento: Agendamentos): Observable<Agendamentos> {
-    return this.http.post<Agendamentos>(`${this.apiUrl}`, agendamento)
+    return this.http.post<Agendamentos>(`${this.apiUrl}`, agendamento, { headers: this.getAuthHeaders() } )
   }
 
   updateAgendamento(id: string, agendamento: Agendamentos): Observable<Agendamentos> {
-    return this.http.put<Agendamentos>(`${this.apiUrl}/${id}`, agendamento)
+    return this.http.put<Agendamentos>(`${this.apiUrl}/${id}`, agendamento, { headers: this.getAuthHeaders() })
   }
 
   deleteAgendamento(ids: string[]): Observable<any> {
-    return this.http.delete(`${this.apiUrl}`, { body: { ids } })
+    return this.http.delete(`${this.apiUrl}`, { 
+      headers: this.getAuthHeaders(),
+      body: { ids } })
   }
 }
